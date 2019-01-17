88a92ddd627c2263192fa8906782c4a6b640d7cf
stan <stan.mwangi@yahoo.com>
2019-01-17 16:28:56 +0300

    added social sign on

--- venturelift_profiles/urls.py
+++ venturelift_profiles/urls.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^accounts/', include('django_registration.backends.activation.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
