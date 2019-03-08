1f195ff502dc5c44cf59f6e511a6a383ee066c9a
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-03-08 12:25:43 +0300

    Business Profile Page :smiley:

--- venturelift_profiles/models.py
+++ venturelift_profiles/models.py
@@ -52,7 +52,7 @@ class Business(models.Model):
     size = models.CharField(max_length=50, choices=BUSINESS_SIZE)
     creator = models.ForeignKey(User, related_name='business_creator')
     thumbnail_image = models.ImageField(
-        upload_to='pic_folder/', default='pic_folder/no-img.jpg')
+        upload_to='pic_folder/')
     company_primary_email = models.EmailField()
     company_secondary_email = models.EmailField(null=True, blank=True)
     facebook_profile = models.URLField(max_length=200, null=True, blank=True)
