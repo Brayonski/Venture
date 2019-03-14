a298b4e128117e40e5e29828c1af9aabf6087faf
stan <stan.mwangi@yahoo.com>
2019-03-14 23:00:39 +0300

    Categories media

--- venturelift_media/urls.py
+++ venturelift_media/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='homepage'),
     url(r'^stories/media/(?P<pk>\d+)/$',
         views.TextMediaView.as_view(), name='media_story'),
+    url(r'^videos/(?P<name>\w+)/$', views.HomeView.as_view(), name='media_videos'),
+    url(r'^podcast/(?P<name>\w+)/$', views.HomeView.as_view(), name='media_podcast'),
 ]
