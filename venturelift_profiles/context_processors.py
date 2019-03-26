ce02d505ad2ed70f594276aa59563dbe6f77dddb
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-03-26 15:17:02 +0300

    MultiSelect Fields :smiley:

--- venturelift_profiles/context_processors.py
+++ venturelift_profiles/context_processors.py
@@ -19,3 +19,17 @@ def account_type(request):
                 investor_profile_id=context['investor'].id)
         return {"context": context}
     return context
+
+
+def verified_account(request):
+    verified_account = False
+    if 'admin' in request.META['PATH_INFO']:
+        return {}
+    if request.user.is_authenticated():
+        if request.user.investor_creator.exists() and Investor.objects.get(user=request.user).verified:
+            verified_account = True
+        if request.user.supporter_creator.exists() and Supporter.objects.get(user=request.user).verified:
+            verified_account = True
+        if request.user.business_creator.exists() and Business.objects.get(creator=request.user).verified:
+            verified_account = True
+    return {"verified": verified_account}
