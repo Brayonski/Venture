54f08aa5cc09e87b158e365da0c9ed6b4aaea028
Tryfiana Wachira <fiona@otbafrica.com>
2019-06-25 16:50:34 +0300

    mpesa task added

--- venturelift_profiles/models.py
+++ venturelift_profiles/models.py
@@ -269,6 +269,7 @@ class BusinessInvestment(models.Model):
     personal_funds_invested = models.IntegerField(null=True, blank=True)
     external_funds_invested = models.IntegerField(null=True, blank=True)
     capital_to_raise = models.IntegerField(null=True, blank=True)
+    current_debt = models.BooleanField(default=False,null=True, blank=True)
 
     class Meta:
         verbose_name = 'Business Investment'
