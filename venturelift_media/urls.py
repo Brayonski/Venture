9cb3bf87512205e5319c0ff8b2793fc954175cf3
Tryfiana Wachira <fiona@otbafrica.com>
2019-07-26 12:44:57 +0300

    venturelift cms add settings

--- venturelift_media/urls.py
+++ venturelift_media/urls.py
@@ -3,8 +3,7 @@ from venturelift_media import views
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='homepage'),
-    url(r'^stories/media/(?P<pk>\d+)/$',
-        views.TextMediaView.as_view(), name='media_story'),
+    url(r'^stories/media/(?P<pk>\d+)/$', views.TextMediaView.as_view(), name='media_story'),
     url(r'^videos/(?P<name>\w+)/$', views.HomeView.as_view(), name='media_videos'),
     url(r'^podcast/(?P<name>\w+)/$', views.HomeView.as_view(), name='media_podcast'),
 ]
