bf0c217a82f3e5eda7a52fac598b4c005f374bf6
Brayonski <kiprotichbrian8@gmail.com>
2020-04-05 02:25:08 +0300

    relative path

--- venturelift_cms/wsgi.py
+++ venturelift_cms/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 import sys
+sys.path.append('F:\work\backup\otbafrica-venturelift-5ee3402e7447\venturelift_cms\settings.py')
 
 from django.core.wsgi import get_wsgi_application
 
